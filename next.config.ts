53679702ad83ca7ca63aef185afc9a67ab5d9a3a
abhinit1999 <krabhinit60@gmail.com>
2026-08-21 08:04:36 +0530

    chore(security): implement strict HTTP security headers and CSP

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.razorpay.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://lh3.googleusercontent.com https://*.supabase.co;
+  font-src 'self' data:;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self' https://checkout.razorpay.com;
+  frame-ancestors 'none';
+  frame-src 'self' https://api.razorpay.com/ https://checkout.razorpay.com/;
+  connect-src 'self' https://*.supabase.co https://checkout.razorpay.com wss://*.supabase.co;
+  upgrade-insecure-requests;
+`
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -12,6 +27,39 @@ const nextConfig: NextConfig = {
         hostname: '*.supabase.co',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
   },
 };
 
